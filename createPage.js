b718fedbfb9c8b1c113c5dc63c96cd4df37ae474
leewonjae <105027084+iwjNitroCell@users.noreply.github.com>
2023-06-02 21:16:39 +0900

    Update createPage.js

--- createPage.js
+++ createPage.js
@@ -3,17 +3,14 @@ const urlParams = url.searchParams;
 
 function init(){
     var parameter = urlParams.get("category");
-    show(parameter);
-    
+    show(parameter); 
 }
 
 function show(name){
-    
     const games = document.getElementsByClassName("game");
     for(var i = 0; i< games.length; i++){
         games[i].style.display = "none";
         for(var j=0; j < games[i].classList.length; j++){
-            console.log(games[i].classList[j])
             if(games[i].classList[j] == name){
                 games[i].style.display = "block";
                 continue;
