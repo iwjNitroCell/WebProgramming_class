e70d0118447dbc00d41d47ce211da3b10ddc35c3
leewonjae <105027084+iwjNitroCell@users.noreply.github.com>
2023-06-02 22:51:26 +0900

    Update questionScript.js

--- questionScript.js
+++ questionScript.js
@@ -3,7 +3,17 @@ const score = [0, 0, 0, 0]
 var result = "";
 var code = 0;
 
+var div;
+var Img;
+var H3;
+var A;
+
 function processing(){
+    div = document.getElementById("resultDiv");
+    Img = div.children[0].children[0];
+    H3 = div.children[1];
+    A = div.children[2];
+
     const url = new URL(window.location.href);
     const urlParams = url.searchParams;
 
@@ -56,58 +66,104 @@ function processing(){
                 break;
         }
     }
+
     
     switch(result){
-        case "ECIL": 
-            code = 1;
+        case "ECIL": // left 4 dead 2  code: 1
+            Img.src = "./img/left_4_dead_2.png";
+            Img.alt = "left 4 dead 2";
+            H3.innerHTML = "Left 4 Dead 2";
+            A.href = "gamePage.html?gamecode=1";
             break;
-        case "HCIS":
-            code = 2;
+        case "HCIS": // rainbow six seige  code: 2
+            Img.src = "./img/raindow_6_seige.png";
+            Img.alt = "rainbow six seige";
+            H3.innerHTML = "Rainbow six Seige";
+            A.href = "gamePage.html?gamecode=2";
             break;
-        case "EAFS":
-            code = 3;
+        case "HAFS": // enter the gungeon  code: 3
+            Img.src = "./img/enter_the_gungeon.png";
+            Img.alt = "enter the gungeon";
+            H3.innerHTML = "Enter the Gungeon";
+            A.href = "gamePage.html?gamecode=3";
             break;
-        case "HAFS":
-            code = 4;
+        case "EAFS": // skul  code: 4
+            Img.src = "./img/shul.png";
+            Img.alt = "skul";
+            H3.innerHTML = "Skul The Hero Slayer";
+            A.href = "gamePage.html?gamecode=4";
             break;
-        case "HCFL":
-            code = 5;
+        case "HCFL": // civilization  code: 5
+            Img.src = "./img/civilization.png";
+            Img.alt = "civilization";
+            H3.innerHTML = "Sid Meier's Civilization VI";
+            A.href = "gamePage.html?gamecode=5";
             break;
-        case "HAFL":
-            code = 6;
+        case "HAFL": // monster hunter  code: 6
+            Img.src = "./img/monster_hunter.png";
+            Img.alt = "monster hunter";
+            H3.innerHTML = "Monster Hunter: World";
+            A.href = "gamePage.html?gamecode=6";
             break;
-        case "ECFS":
-            code = 7;
+        case "ECFS": // human: fall flat  code: 7
+            Img.src = "./img/human_fall_flat.png";
+            Img.alt = "human: fall flat";
+            H3.innerHTML = "Human: Fall Flat";
+            A.href = "gamePage.html?gamecode=7";
             break;
-        case "HAIS":
-            code = 8;
+        case "HAIS": // hunt: showdown  code: 8
+            Img.src = "./img/hunt_showdown.png";
+            Img.alt = "hunt: showdown";
+            H3.innerHTML = "Hunt: Showdown";
+            A.href = "gamePage.html?gamecode=8";
             break;
-        case "ECFL":
-            code = 9;
+        case "ECFL": // ark survival  code: 9
+            Img.src = "./img/ark_survival.png";
+            Img.alt = "ark survival";
+            H3.innerHTML = "ARK: Survival Evolved";
+            A.href = "gamePage.html?gamecode=9";
             break;
-        case "EAFL":
-            code = 10;
+        case "EAFL": // stardew valley  code: 10
+            Img.src = "./img/stardew_valley.png";
+            Img.alt = "stardew valley";
+            H3.innerHTML = "Stardew Valley";
+            A.href = "gamePage.html?gamecode=10";
             break;
-        case "ECIS":
-            code = 11;
+        case "ECIS": // hearthstone  code: 11
+            Img.src = "./img/hearthstone.png";
+            Img.alt = "hearthstone";
+            H3.innerHTML = "HearthStone";
+            A.href = "gamePage.html?gamecode=11";
             break;
-        case "EAIS":
-            code = 12;
+        case "EAIS": // slay the spire  code: 12
+            Img.src = "./img/slay_the_spire.png";
+            Img.alt = "slay the spire";
+            H3.innerHTML = "Slay the Spire";
+            A.href = "gamePage.html?gamecode=12";
             break;
-        case "HCIL":
-            code = 13;
+        case "HCIL": // potal2  code: 13
+            Img.src = "./img/portal2.png";
+            Img.alt = "potal2";
+            H3.innerHTML = "Portal 2";
+            A.href = "gamePage.html?gamecode=13";
             break;
-        case "EAIL":
-            code = 14;
+        case "EAIL": // vampire survivors  code: 14
+            Img.src = "./img/vampire_survivors.png";
+            Img.alt = "vampire survivors";
+            H3.innerHTML = "Vampire Survivors";
+            A.href = "gamePage.html?gamecode=14";
             break;
-        case "HCFS":
-            code = 15;
+        case "HCFS": // for the king  code: 15
+            Img.src = "./img/for_the_king.png";
+            Img.alt = "for the king";
+            H3.innerHTML = "For The King";
+            A.href = "gamePage.html?gamecode=15";
             break;
-        case "HAIL":
-            code = 16;
+        case "HAIL": // there is no game  code: 16
+            Img.src = "./img/there_is_no_game.png";
+            Img.alt = "there is no game";
+            H3.innerHTML = "There Is No Game: Wrong Dimension";
+            A.href = "gamePage.html?gamecode=16";
             break;
-    } 
+    }
 }
-
-
-
