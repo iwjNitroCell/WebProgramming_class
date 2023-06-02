529c735f7ed7f833eedce1843bea7ae798dd3c01
leewonjae <105027084+iwjNitroCell@users.noreply.github.com>
2023-06-03 00:16:57 +0900

    Update questionScript.js

--- questionScript.js
+++ questionScript.js
@@ -70,97 +70,97 @@ function processing(){
     
     switch(result){
         case "ECIL": // left 4 dead 2  code: 1
-            Img.src = "img/left_4_dead_2.png";
+            Img.src = "img/left_4_dead_2.PNG";
             Img.alt = "left 4 dead 2";
             H3.innerHTML = "Left 4 Dead 2";
             A.href = "gamePage.html?gamecode=1";
             break;
         case "HCIS": // rainbow six seige  code: 2
-            Img.src = "img/raindow_6_seige.png";
+            Img.src = "img/raindow_6_seige.PNG";
             Img.alt = "rainbow six seige";
             H3.innerHTML = "Rainbow six Seige";
             A.href = "gamePage.html?gamecode=2";
             break;
         case "HAFS": // enter the gungeon  code: 3
-            Img.src = "img/enter_the_gungeon.png";
+            Img.src = "img/enter_the_gungeon.PNG";
             Img.alt = "enter the gungeon";
             H3.innerHTML = "Enter the Gungeon";
             A.href = "gamePage.html?gamecode=3";
             break;
         case "EAFS": // skul  code: 4
-            Img.src = "img/shul.png";
+            Img.src = "img/shul.PNG";
             Img.alt = "skul";
             H3.innerHTML = "Skul The Hero Slayer";
             A.href = "gamePage.html?gamecode=4";
             break;
         case "HCFL": // civilization  code: 5
-            Img.src = "img/civilization.png";
+            Img.src = "img/civilization.PNG";
             Img.alt = "civilization";
             H3.innerHTML = "Sid Meier's Civilization VI";
             A.href = "gamePage.html?gamecode=5";
             break;
         case "HAFL": // monster hunter  code: 6
-            Img.src = "img/monster_hunter.png";
+            Img.src = "img/monster_hunter.PNG";
             Img.alt = "monster hunter";
             H3.innerHTML = "Monster Hunter: World";
             A.href = "gamePage.html?gamecode=6";
             break;
         case "ECFS": // human: fall flat  code: 7
-            Img.src = "img/human_fall_flat.png";
+            Img.src = "img/human_fall_flat.PNG";
             Img.alt = "human: fall flat";
             H3.innerHTML = "Human: Fall Flat";
             A.href = "gamePage.html?gamecode=7";
             break;
         case "HAIS": // hunt: showdown  code: 8
-            Img.src = "img/hunt_showdown.png";
+            Img.src = "img/hunt_showdown.PNG";
             Img.alt = "hunt: showdown";
             H3.innerHTML = "Hunt: Showdown";
             A.href = "gamePage.html?gamecode=8";
             break;
         case "ECFL": // ark survival  code: 9
-            Img.src = "img/ark_survival.png";
+            Img.src = "img/ark_survival.PNG";
             Img.alt = "ark survival";
             H3.innerHTML = "ARK: Survival Evolved";
             A.href = "gamePage.html?gamecode=9";
             break;
         case "EAFL": // stardew valley  code: 10
-            Img.src = "img/stardew_valley.png";
+            Img.src = "img/stardew_valley.PNG";
             Img.alt = "stardew valley";
             H3.innerHTML = "Stardew Valley";
             A.href = "gamePage.html?gamecode=10";
             break;
         case "ECIS": // hearthstone  code: 11
-            Img.src = "img/hearthstone.png";
+            Img.src = "img/hearthstone.jpg";
             Img.alt = "hearthstone";
             H3.innerHTML = "HearthStone";
             A.href = "gamePage.html?gamecode=11";
             break;
         case "EAIS": // slay the spire  code: 12
-            Img.src = "img/slay_the_spire.png";
+            Img.src = "img/slay_the_spire.PNG";
             Img.alt = "slay the spire";
             H3.innerHTML = "Slay the Spire";
             A.href = "gamePage.html?gamecode=12";
             break;
         case "HCIL": // potal2  code: 13
-            Img.src = "img/portal2.png";
+            Img.src = "img/portal2.PNG";
             Img.alt = "potal2";
             H3.innerHTML = "Portal 2";
             A.href = "gamePage.html?gamecode=13";
             break;
         case "EAIL": // vampire survivors  code: 14
-            Img.src = "img/vampire_survivors.png";
+            Img.src = "img/vampire_survivors.PNG";
             Img.alt = "vampire survivors";
             H3.innerHTML = "Vampire Survivors";
             A.href = "gamePage.html?gamecode=14";
             break;
         case "HCFS": // for the king  code: 15
-            Img.src = "img/for_the_king.png";
+            Img.src = "img/for_the_king.PNG";
             Img.alt = "for the king";
             H3.innerHTML = "For The King";
             A.href = "gamePage.html?gamecode=15";
             break;
         case "HAIL": // there is no game  code: 16
-            Img.src = "img/there_is_no_game.png";
+            Img.src = "img/there_is_no_game.PNG";
             Img.alt = "there is no game";
             H3.innerHTML = "There Is No Game: Wrong Dimension";
             A.href = "gamePage.html?gamecode=16";
